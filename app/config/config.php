a98edf4ddea4717b296584aa53bb635b71287d6d
Alfan <alfan.wahyudi98@gmail.com>
2020-09-10 15:52:23 +0700

    kerangka MVC web

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASEURL', 'http://localhost/rest-api-dishub-bandung');
+
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'dishub-bandung');
